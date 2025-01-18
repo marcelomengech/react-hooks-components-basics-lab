ca89596ce99935215db64673b0a5962e62be1dd2
marcelomengech <marcelomengech@gmail.com>
2025-01-18 21:41:00 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./components/App";
+import App from "./components/App"; // Make sure your App component is in the correct path
 
 ReactDOM.render(<App />, document.getElementById("root"));
